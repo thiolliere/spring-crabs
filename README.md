ed7d3dd0377be652641286e2366f06c673cbef09
thiolliere <guillaume.thiolliere@opmbx.org>
2017-05-03 22:15:51 +0200

    more complexe example with issue on rust beta too

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Issue using glutin and serde:
+Issue using glutin and serde in emscripten:
 
-if you delete line main.rs:30: `let cond = CFG.cond;` it works ...
+when running on the web:
+```
+Successfully compiled asm.js code (total compilation time 2994ms; storage initialization failed (consider filing a bug); 6 functions compiled slowly: ql:8:8 (433ms), Rf:12:38706 (391ms), Fg:13:8 (409ms), hl:15:8 (661ms), Ci:9:8 (2079ms), pk:11:8 (2472ms)) spring-crabs.js
+emscripten_webgl_create_context failed: explicitSwapControl is not supported, please rebuild with -s OFFSCREENCANVAS_SUPPORT=1 to enable targeting the experimental OffscreenCanvas specification! spring-crabs.js:1:471520
 
+thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: BackendCreationError(OsError("Error while calling emscripten_webgl_create_context: Internal error in the library (success detected as failure)"))', /checkout/src/libcore/result.rs:859 index.htm:141:13
 
+note: Run with `RUST_BACKTRACE=1` for a backtrace. index.htm:141:13
+```
+
+if you delete CFG.window.fullscreen static ref call in the main then it works.
--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,88 @@ extern crate serde_derive;
 extern crate serde;
 extern crate toml;
 
-#[derive(Deserialize)]
-struct Configuration {
-    cond: bool,
-}
-
-lazy_static! {
-    static ref CFG: Configuration = toml::from_str("cond = true").unwrap();
-}
-
 use std::cell::RefCell;
 use std::ptr::null_mut;
 use std::os::raw::{c_int, c_void};
 
 use glium::{DisplayBuild, Surface};
 
+#[derive(Deserialize)]
+pub struct Configuration {
+    pub window: Window,
+    pub gameplay: Gameplay,
+    pub graphics: Graphics,
+    pub camera: Camera,
+    pub event_loop: EventLoop,
+    pub control: Control,
+    pub physics: Physics,
+    pub audio: Audio,
+}
+
+#[derive(Deserialize)]
+pub struct Control {
+    pub mouse_sensibility: f64,
+}
+#[derive(Deserialize)]
+pub struct Window {
+    pub samples: u8,
+    pub fullscreen: bool,
+    pub vsync: bool,
+    pub dimensions: [u32; 2],
+}
+#[derive(Deserialize)]
+pub struct Gameplay {
+    pub gravity: f64,
+    pub ball_radius: f64,
+    pub damping: f64,
+    pub impulse: f64,
+    pub reset: bool,
+}
+#[derive(Deserialize)]
+pub struct Graphics {
+    pub ball_color: [f32; 4],
+    pub wall_color: [f32; 4],
+    pub background_color: [f32; 4],
+    pub cursor_color: [f32; 4],
+    pub cursor_inner_radius: f32,
+    pub cursor_outer_radius: f32,
+    pub cursor_thickness: f32,
+    pub effect_timer: f64,
+    pub effect_color: [f32; 4],
+    pub effect_thickness: f32,
+}
+#[derive(Deserialize)]
+pub struct Camera {
+    pub zoom: f64,
+}
+#[derive(Deserialize)]
+pub struct EventLoop {
+    pub max_fps: u32,
+}
+#[derive(Deserialize)]
+pub struct Physics {
+    pub unit: f64,
+}
+#[derive(Deserialize)]
+pub struct Audio {
+    pub jump_volume: f32,
+    pub wall_volume: f32,
+
+    pub wall_max_intensity: f64,
+    pub wall_min_intensity: f64,
+}
+
+lazy_static! {
+    pub static ref CFG: Configuration = toml::from_str(include_str!("../config.toml")).unwrap();
+}
+
 fn main() {
     let mut builder = glium:: glutin::WindowBuilder::new()
-        .with_fullscreen(glium::glutin::get_primary_monitor())
         .with_depth_buffer(24);
 
-    let cond = CFG.cond;
+    if CFG.window.fullscreen {
+        builder = builder.with_fullscreen(glium::glutin::get_primary_monitor());
+    }
 
     let display = builder.build_glium().unwrap();
     display.get_window().unwrap().set_cursor_state(glium::glutin::CursorState::Grab).unwrap();
