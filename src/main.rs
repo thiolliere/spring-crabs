0606c6336b1f4b0589e1835cc9071366a780a3a7
thiolliere <guillaume.thiolliere@opmbx.org>
2017-05-03 23:26:45 +0200

    more complexe example
    it is very strange before this commit it seem to fail sometimes but not
    always depending on the compilation.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
-extern crate toml;
+
+mod configuration;
 
 use std::cell::RefCell;
 use std::ptr::null_mut;
@@ -13,80 +14,11 @@ use std::os::raw::{c_int, c_void};
 
 use glium::{DisplayBuild, Surface};
 
-#[derive(Deserialize)]
-pub struct Configuration {
-    pub window: Window,
-    pub gameplay: Gameplay,
-    pub graphics: Graphics,
-    pub camera: Camera,
-    pub event_loop: EventLoop,
-    pub control: Control,
-    pub physics: Physics,
-    pub audio: Audio,
-}
-
-#[derive(Deserialize)]
-pub struct Control {
-    pub mouse_sensibility: f64,
-}
-#[derive(Deserialize)]
-pub struct Window {
-    pub samples: u8,
-    pub fullscreen: bool,
-    pub vsync: bool,
-    pub dimensions: [u32; 2],
-}
-#[derive(Deserialize)]
-pub struct Gameplay {
-    pub gravity: f64,
-    pub ball_radius: f64,
-    pub damping: f64,
-    pub impulse: f64,
-    pub reset: bool,
-}
-#[derive(Deserialize)]
-pub struct Graphics {
-    pub ball_color: [f32; 4],
-    pub wall_color: [f32; 4],
-    pub background_color: [f32; 4],
-    pub cursor_color: [f32; 4],
-    pub cursor_inner_radius: f32,
-    pub cursor_outer_radius: f32,
-    pub cursor_thickness: f32,
-    pub effect_timer: f64,
-    pub effect_color: [f32; 4],
-    pub effect_thickness: f32,
-}
-#[derive(Deserialize)]
-pub struct Camera {
-    pub zoom: f64,
-}
-#[derive(Deserialize)]
-pub struct EventLoop {
-    pub max_fps: u32,
-}
-#[derive(Deserialize)]
-pub struct Physics {
-    pub unit: f64,
-}
-#[derive(Deserialize)]
-pub struct Audio {
-    pub jump_volume: f32,
-    pub wall_volume: f32,
-
-    pub wall_max_intensity: f64,
-    pub wall_min_intensity: f64,
-}
-
-lazy_static! {
-    pub static ref CFG: Configuration = toml::from_str(include_str!("../config.toml")).unwrap();
-}
-
 fn main() {
     let mut builder = glium:: glutin::WindowBuilder::new()
         .with_depth_buffer(24);
 
-    if CFG.window.fullscreen {
+    if configuration::CFG.window.fullscreen {
         builder = builder.with_fullscreen(glium::glutin::get_primary_monitor());
     }
 
